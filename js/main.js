18a894db04d56cab6a563a43eb5dfb64e2307c9b
nealpointerexception <neal20code@gmail.com>
2020-07-02 15:32:08 -0700

    added background colors, tilt and links

--- js/main.js
+++ js/main.js
@@ -1,58 +1,60 @@
 window.onload
     = function () {
-    Particles.init
-    ({
+        Particles.init
+            ({
 
-// normal options
-        selector:
-            '.background'
-        ,
-        maxParticles:
-            100
-        ,
-        connectParticles:
-            true,
-        color:
-            '#868686'
-        ,
-// options for breakpoints
-        responsive: [
-            {
-                breakpoint:
-                    768
-                ,
-                options: {
-                    maxParticles:
-                        100
-                    ,
-                    color:
-                        '#999999'
-                    ,
-                    connectParticles:
-                        true
-                }
-            }, {
-                breakpoint:
-                    425
-                ,
-                options: {
-                    maxParticles:
-                        100
-                    ,
-                    connectParticles:
-                        true
-                }
-            }, {
-                breakpoint:
-                    320
-                ,
-                options: {
-                    maxParticles:
-                        0
+                // normal options
+                selector:'.background',
+                maxParticles:100,
+                connectParticles:true,
+                color:['#868686', '#ffffff', '#cc1ba9', '#5f00e3'],
+                sizeVariations: 5,
 
-// disables particles.js
-                }
-            }
-        ]
-    });
-};
+                // options for breakpoints
+                responsive: [
+                    {
+                        breakpoint:
+                            768
+                        ,
+                        options: {
+                            maxParticles:
+                                100
+                            ,
+                            color:
+                                '#999999'
+                            ,
+                            connectParticles:
+                                true
+                        }
+                    }, {
+                        breakpoint:
+                            425
+                        ,
+                        options: {
+                            maxParticles:
+                                100
+                            ,
+                            connectParticles:
+                                true
+                        }
+                    }, {
+                        breakpoint:
+                            320
+                        ,
+                        options: {
+                            maxParticles:
+                                0
+
+                            // disables particles.js
+                        }
+                    }
+                ]
+            });
+    };
+    $(document).ready(function(){
+        $('.js-tilt').tilt({
+            // glare: true,
+            // maxGlare: 1,
+            scale: 1.03,
+        })
+      });
